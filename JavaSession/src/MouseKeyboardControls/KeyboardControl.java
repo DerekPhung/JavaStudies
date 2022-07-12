ef83bf2490bf75e12bb585c21353ea74aa747d90
Vi Nguyen <fluffywin@gmail.com>
2022-07-12 11:57:16 -0400

    added electrics and law combo for keyboard

--- JavaSession/src/MouseKeyboardControls/KeyboardControl.java
+++ JavaSession/src/MouseKeyboardControls/KeyboardControl.java
@@ -6,6 +6,15 @@ import java.awt.event.KeyEvent;
 public class KeyboardControl {
 
     static Robot ai;
+    static int up = KeyEvent.VK_W;
+    static int down = KeyEvent.VK_S;
+    static int left = KeyEvent.VK_A;
+    static int right = KeyEvent.VK_D;
+
+    static int btn1 = KeyEvent.VK_U;
+    static int btn2 = KeyEvent.VK_I;
+    static int btn3 = KeyEvent.VK_J;
+    static int btn4 = KeyEvent.VK_K;
 
     static {
         try {
@@ -22,6 +31,60 @@ public class KeyboardControl {
     // going to figure out a way to not hardcode this
     public static void main(String[] args) throws Exception{
 
+        //punch u i
+        //kick j k
+        var up = KeyEvent.VK_W;
+        var down = KeyEvent.VK_S;
+        var left = KeyEvent.VK_A;
+        var right = KeyEvent.VK_D;
+
+        var btn1 = KeyEvent.VK_U;
+        var btn2 = KeyEvent.VK_I;
+        var btn3 = KeyEvent.VK_J;
+        var btn4 = KeyEvent.VK_K;
+
+        KeyboardControl key = new KeyboardControl();
+
+        Thread.sleep(2000);
+
+        for(int i = 0; i < 10; i++) {
+            electrics();
+        }
+
+        //ending taunt
+        Thread.sleep(1000);
+        ai.keyPress(KeyEvent.VK_DELETE);
+        Thread.sleep(100);
+        ai.keyRelease(KeyEvent.VK_DELETE);
+
+        System.out.println("done");
+
+
+    }
+
+    static public void electrics() throws InterruptedException {
+        //eletric
+        //f
+        ai.keyPress(right);
+        Thread.sleep(50);
+        ai.keyRelease(right);
+        Thread.sleep(100);
+
+        //df+2
+        ai.keyPress(down);
+        ai.keyPress(right);
+        ai.keyPress(btn2);
+
+        Thread.sleep(100);
+
+        ai.keyRelease(btn2);
+        ai.keyRelease(down);
+        ai.keyRelease(right);
+
+        Thread.sleep(500);
+    }
+
+    public void SampleCombo15() throws InterruptedException, AWTException {
         //punch u i
         //kick j k
         var up = KeyEvent.VK_W;
@@ -158,12 +221,6 @@ public class KeyboardControl {
         ai.keyRelease(KeyEvent.VK_DELETE);
 
         System.out.println("done");
-
-
-    }
-
-    public void SampleCombo15(){
-
     }
 
 
